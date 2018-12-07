824b59435518b5a3bab6366d8e163bb881161538
sanyuktaghai <sanyukta22@gmail.com>
2018-12-07 15:11:16 -0800

    Week and weekend model and rolling mean model

--- code/airlineForecast.py
+++ code/airlineForecast.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pylab as plt
+#%% matplotlib inline
+from matplotlib.pylab import rcParams
+rcParams['figure.figsize'] = 15, 6
 
 def airlineForecast(trainingDataFileName, validationDataFileName):
 
@@ -11,6 +15,10 @@ def airlineForecast(trainingDataFileName, validationDataFileName):
     getHistoricalMultiplicativeDemand(historical_data, valid_data)
     getWeeklyAdditiveDemand(historical_data, valid_data)
     getWeeklyMultiplicativeDemand(historical_data, valid_data)
+    getWeekVsWeekendAdditiveDemand(historical_data, valid_data)
+    getWeekVsWeekendMultiplicativeDemand(historical_data, valid_data)
+    getRollingMeanAdditiveDemand(historical_data, valid_data)
+    getRollingMeanMultiplicativeDemand(historical_data, valid_data)
     
 def getTrainedDataset(trainingDataFileName):
     # import data
@@ -70,6 +78,50 @@ def getWeeklyMultiplicativeDemand(historical_data, valid_data):
     multiplicative_data = multiplicative_data[multiplicative_data.days_prior != 0]
     multiplicative_data["final_forecast"] = multiplicative_data["cum_bookings"]/multiplicative_data["multiplicative_rate"]
     print("MASE - Weekly Multiplicative Model " + str(calculateMASE(multiplicative_data)))
+
+def getWeekVsWeekendAdditiveDemand(historical_data, valid_data):
+    historical_data["net_book"] = historical_data["final_data"] - historical_data["cum_bookings"]
+    historical_data["day_of_week"] = (historical_data["booking_date"]).dt.dayofweek
+    historical_data["is_weekend"] = historical_data['day_of_week'].apply(lambda x: 'True' if x in [0,6] else 'False')
+    historical_data = historical_data[["days_prior", "is_weekend", "net_book"]]
+    historical_data = historical_data.groupby(["days_prior","is_weekend"]).mean().reset_index()
+    valid_data["is_weekend"] = valid_data['day_of_week'].apply(lambda x: 'True' if x in [0,6] else 'False')
+    additive_data = pd.merge(valid_data, historical_data, how='left', on=["days_prior", "is_weekend"])
+    additive_data = additive_data[additive_data.days_prior != 0]
+    additive_data["final_forecast"] = additive_data["cum_bookings"]+ additive_data["net_book"]
+    print("MASE - Week vs Weekend  Additive Model " + str(calculateMASE(additive_data)))
+
+def getWeekVsWeekendMultiplicativeDemand(historical_data, valid_data):
+    historical_data["multiplicative_rate"] = historical_data["cum_bookings"]/historical_data["final_data"]
+    historical_data["day_of_week"] = (historical_data["booking_date"]).dt.dayofweek
+    historical_data["is_weekend"] = historical_data['day_of_week'].apply(lambda x: 'True' if x in [0,6] else 'False')
+    historical_data = historical_data[["days_prior", "is_weekend", "multiplicative_rate"]]
+    historical_data = historical_data.groupby(["days_prior","is_weekend"]).mean().reset_index()
+    valid_data["is_weekend"] = valid_data['day_of_week'].apply(lambda x: 'True' if x in [0,6] else 'False')
+    multiplicative_data = pd.merge(valid_data, historical_data, how='left', on=["days_prior", "is_weekend"])
+    multiplicative_data = multiplicative_data[multiplicative_data.days_prior != 0]
+    multiplicative_data["final_forecast"] = multiplicative_data["cum_bookings"]/multiplicative_data["multiplicative_rate"]
+    print("MASE - Week vs Weekend Multiplicative Model " + str(calculateMASE(multiplicative_data)))
+
+def getRollingMeanAdditiveDemand(historical_data, valid_data):
+    historical_data["net_book"] = historical_data["final_data"] - historical_data["cum_bookings"]
+    historical_data = historical_data[["days_prior", "net_book"]]
+    test1 = historical_data.groupby('days_prior')['net_book'].apply(lambda x:x.rolling(center=True,window=3).mean())
+    historical_data = test1.groupby(historical_data["days_prior"]).mean().reset_index()
+    additive_data = pd.merge(valid_data, historical_data, how='left', on="days_prior")
+    additive_data = additive_data[additive_data.days_prior != 0]
+    additive_data["final_forecast"] = additive_data["cum_bookings"]+ additive_data["net_book"]
+    print("MASE - Rolling Mean Additive Model " + str(calculateMASE(additive_data)))
+
+def getRollingMeanMultiplicativeDemand(historical_data, valid_data):
+    historical_data["multiplicative_rate"] = historical_data["cum_bookings"]/historical_data["final_data"]
+    historical_data = historical_data[["days_prior", "multiplicative_rate"]]
+    test1 = historical_data.groupby('days_prior')['multiplicative_rate'].apply(lambda x:x.rolling(center=True,window=2).mean())
+    historical_data = test1.groupby(historical_data["days_prior"]).mean().reset_index()
+    multiplicative_data = pd.merge(valid_data, historical_data, how='left', on=["days_prior"])
+    multiplicative_data = multiplicative_data[multiplicative_data.days_prior != 0]
+    multiplicative_data["final_forecast"] = multiplicative_data["cum_bookings"]/multiplicative_data["multiplicative_rate"]
+    print("MASE - Rolling Mean Multiplicative Model " + str(calculateMASE(multiplicative_data)))
 
 def getFinalBookingPerDepartureDate(final_data):
     final_data = final_data.loc[final_data['days_prior'] == 0]
